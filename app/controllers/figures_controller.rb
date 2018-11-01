554b535f3f83a8cabf6d81f54ec52c14607e5c33
James Lee <jameslee1187@gmail.com>
2018-11-01 18:00:57 -0400

    yes

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -13,14 +13,29 @@ class FiguresController < ApplicationController
   end
 
   post '/figures' do
+    # binding.pry
     @figure = Figure.create(params['figure'])
-    unless params[:landmark][:name].empty?
-      @figure.landmarks << Landmark.create(params[:landmark])
-    end
-    unless params[:title][:name].empty?
-      @figure.titles << Title.create(params[:title])
-    end
+    title = Title.create(params[:title])
+    FigureTitle.create(figure: @figure, title: title)
+    # binding.pry
+    landmark = Landmark.create(params[:landmark])
+    @figure.landmarks << landmark
     @figure.save
     redirect to "figures/#{@figure.id}"
   end
+
+  get "/figures/:id" do
+    # binding.pry
+    @figure = Figure.find(params[:id])
+    erb :"figures/show"
+  end
+
+  get "/figures/:id/edit" do
+    @figure = Figure.find(params[:id])
+    @titles = Titles.all
+    binding.pry
+    erb :"figures/edit"
+  end
+
+
 end
